08ad8994896a51cdd1229b06e88c7ee2b3e31044
zhouhaoxiang <zhouhaoxiangcn@qq.com>
2018-06-27 17:06:58 +0800

    6.26

--- swordOffer/23.js
+++ swordOffer/23.js
@@ -1,2 +1,39 @@
 // 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果
 //。如果是则输出Yes,否则输出No。假设输入的数组的任意两个数字都互不相同。
+
+function VerifySquenceOfBST(sequence)
+{
+    // write code here
+    if(sequence.length === 1){
+        return true;
+    } else if(sequence.length === 0){
+        return false
+    }
+    return isBST(sequence)
+}
+
+function isBST(arr) {
+    if(arr.length <= 2){
+        return true
+    } else {
+        let after = arr.pop();
+        let turn = false;
+        let turnIndex = -1;
+        for(let i = 0,len = arr.length; i < len; i++){
+            if(turn && arr[i] < after){
+                return false;
+            }
+            if(arr[i] > after && !turn){
+                turn = true;
+                turnIndex = i;
+            } 
+        }
+        if(turnIndex === -1){
+            return isBST(arr)
+        } else{
+            let preArr = arr.slice(0, turnIndex);
+            let afterArr = arr.slice(turnIndex);
+            return isBST(preArr) && isBST(afterArr);
+        }
+    }
+}
---
+++ swordOffer/24.js
@@ -0,0 +1,30 @@
+//  输入一颗二叉树和一个整数，打印出二叉树中结点值的和为输入整数的所有路径
+// 。路径定义为从树的根结点开始往下一直到叶结点所经过的结点形成一条路径
+
+// 这个 null 应该不用加啊
+
+function FindPath(root, expectNumber) {
+    if(root === null){
+        return []
+    }
+    let ans = [];
+    search(root, [], expectNumber, ans)
+    return ans
+}
+
+function search(node, arr, expectNumber,ans) {
+    let newArr = arr.slice(0);
+    newArr.push(node.val)
+    expectNumber = expectNumber - node.val;
+    if (expectNumber === 0 && node.left === null && node.right === null) {  
+        ans.push(newArr)
+    } else {
+        if (node.left) {
+           search(node.left, newArr, expectNumber, ans);
+        }
+        if (node.right) {
+           search(node.right, newArr, expectNumber, ans);
+        }
+    }
+}
+
