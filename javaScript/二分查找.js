bdecde859b727121945e92f995d49d6898f15235
zhouhaoxiang <zhouhaoxiangcn@qq.com>
2018-06-27 22:40:04 +0800

    6.27

--- javaScript/二分查找.js
+++ javaScript/二分查找.js
@@ -1,9 +1,19 @@
-function BinarySearch(arr) {
-    let left = 0;
+function BinarySearch(arr, item) { // arr 是排序好的
+    let left = 0; 
     let right = arr.length - 1;
+    let mid = -1
 
-    
-
+    while(left <= right) {
+        mid = Math.floor(left + right / 2);
+        if(arr[mid] === item) {
+            return mid;
+        } else if(arr[mid] < item) {
+            left = mid - 1;
+        } else if(arr[mid] > item) {
+            right = mid + 1;
+        }  
+    }
+    return  -1
 }
 
 
